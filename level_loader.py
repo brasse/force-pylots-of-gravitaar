e86acaf7ee93559a3076c2b5ffa8a1e1a05f68eb
offe <oscar.lindberg@gmail.com>
2009-03-27 19:26:14 +0100

    Start work on new level loader

--- level_loader.py
+++ level_loader.py
@@ -4,21 +4,9 @@ from svg_paths import linearize_path, path_area, reverse_path, path_points, tria
 
 from xml.dom import minidom, Node
 
-UP = object()
-DOWN = object()
-
-def node_iter(node):
-    yield node
-    yield DOWN
-    for child in node.childNodes:
-        for cn in node_iter(child):
-            yield cn
-    yield UP
-
-def element_iter(dom):
-    for n in node_iter(dom):
-        if n in [UP, DOWN] or n.nodeType == Node.ELEMENT_NODE:
-            yield n
+################
+# Common stuff #
+################
 
 def str_to_dict(s, pair_sep=';', key_value_sep=':'):
     def true_tuple(k, v=True):
@@ -43,6 +31,107 @@ def shape_common(e):
     sd = str_to_dict(e.getAttribute('style'))
     return id, label, sd
 
+def get_winning_condition(node):
+    wc_attr = node.getAttribute('winning_condition')
+    return [signal.strip() for signal in wc_attr.split(',') if signal != '']
+  
+#############
+# New ztuff #
+#############
+
+def handle_node_svg(node, header, bodies, transform):
+    header.update(width=float(node.getAttribute('width')), 
+                  height=float(node.getAttribute('height')),
+                  winning_condition=get_winning_condition(node))
+    def transform(x, y):
+        return x, header['height'] - y
+    parse_children(node, header, bodies, transform)
+
+def handle_node_namedview(node, header, bodies, transform):
+    bodies.append(('pagecolor', node.getAttribute('pagecolor')))
+    parse_children(node, header, bodies, transform)
+
+def handle_node_rect(node, header, bodies, transform):
+    id, label, sd = shape_common(node)
+    x, y, w, h = [float(node.getAttribute(n)) for n in ['x', 'y', 'width', 'height']]
+    bodies.append((id, label, [('rect', id, label, sd, (transform(x, y + h), (w, h)))]))
+
+def handle_node_path(node, header, bodies, transform):
+    if node.getAttribute('sodipodi:type') == 'arc':
+        id, label, sd = shape_common(node)
+        x, y, rx, ry = [float(node.getAttribute('sodipodi:'+n))
+                        for n in ['cx', 'cy', 'rx', 'ry']]
+        bodies.append((id, label, [('circle', id, label, sd, (transform(x, y), (rx, ry)))]))
+    else:
+        id, label, sd = shape_common(node)
+        path = node.getAttribute('d')
+        path = linearize_path(path)
+        # Make sure that closed paths are defined counter clockwise
+        if path.split()[-1] == 'z' and path_area(path) > 0.0:
+            path = reverse_path(path)
+        is_polygon = path.split()[-1] == 'z'
+        name = 'polygon' if is_polygon else 'path'
+        if is_polygon and 'triangulate' in label:
+            convex_path = triangulate_path(path)
+            paths = split_paths(convex_path)
+        else:
+            paths = [path]
+        parts = []
+        for path in paths:
+            points = path_points(path)
+            points = [transform(x,y) for x, y in points]
+            parts.append((name, id, label, sd, points))
+        #return [(name, id, label, sd, points)]
+        bodies.append((id, label, parts))
+
+def handle_node_default(node, header, bodies, transform):
+    print node.nodeName
+    parse_children(node, header, bodies, transform)
+
+def parse_children(node, header, bodies, transform):
+    for child in node.childNodes:
+        parse_subtree(child, header, bodies, transform)
+
+def parse_subtree(node, header, bodies, transform):
+    handlers = {
+            'svg': handle_node_svg, 
+            'path': handle_node_path, 
+            'rect': handle_node_rect, 
+            'sodipodi:namedview': handle_node_namedview
+            }
+    handlers.get(node.nodeName, handle_node_default)(node, header, bodies, transform)
+
+def read_level2(file):
+    root = minidom.parse(file)
+    header = {}
+    bodies = []
+
+    parse_subtree(root, header, bodies, None)
+    
+    #header, bodies = read_level(file)
+    return header, bodies
+
+
+#############
+# Old stuff #
+#############
+
+UP = object()
+DOWN = object()
+
+def node_iter(node):
+    yield node
+    yield DOWN
+    for child in node.childNodes:
+        for cn in node_iter(child):
+            yield cn
+    yield UP
+
+def element_iter(dom):
+    for n in node_iter(dom):
+        if n in [UP, DOWN] or n.nodeType == Node.ELEMENT_NODE:
+            yield n
+
 def get_transform(e):
     t = e.getAttribute('transform')
     if t.startswith('translate'):
@@ -58,7 +147,6 @@ def do_transform(t, ps):
 #http://math.univ-lille1.fr/~barraud/Inkscape/pathdeform/simpletransform.py
 #http://www.w3.org/TR/SVG/coords.html
 #http://apike.ca/prog_svg_transform.html
-
 
 MULTISHAPE = object()
 
@@ -149,11 +237,7 @@ def just_bodies(height, es):
             yield id, label, [(type, id, label, sd, geometry)]
         else:
             yield body
-
-def get_winning_condition(node):
-    wc_attr = node.getAttribute('winning_condition')
-    return [signal.strip() for signal in wc_attr.split(',') if signal != '']
-    
+  
 def read_level(file):
     dom = minidom.parse(file)
     i = element_iter(dom)
@@ -167,12 +251,19 @@ def read_level(file):
 
 def main(argv):
     import pprint
+    with open(argv[1]) as f2:
+        header2, bodies2 = read_level2(f2)
+    print bodies2
     with open(argv[1]) as f:
         header, bodies = read_level(f)
-    pprint.pprint(header)
+    print header == header2
+    #pprint.pprint(header)
     print
-    for b in bodies:
+    for i, b in enumerate(bodies):
         pprint.pprint(b)
+        print
+        pprint.pprint(bodies2[i])
+        print i, b == bodies2[i]
 
 if __name__ == '__main__':
     import sys

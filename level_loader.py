6111c4e33c0c4c8466271a020a159a167a4e35c9
brasse <thebrasse@brasse.org>
2009-04-01 23:39:08 +0200

    Implemented inherited labels.

--- level_loader.py
+++ level_loader.py
@@ -2,10 +2,25 @@ from __future__ import with_statement
 
 from svg_paths import linearize_path, path_area, reverse_path, path_points, triangulate_path, split_paths
 
+import copy
 import re
 from xml.dom import minidom, Node
 
 # Utils
+
+class LabelStack(object):
+    def __init__(self):
+        self.stack = [{}]
+
+    def push(self, label):
+        self.stack.append(copy.copy(self.stack[-1]))
+        self.stack[-1].update(label)
+
+    def pop(self):
+        self.stack.pop()
+
+    def __call__(self):
+        return self.stack[-1]
 
 class Transform(object):
     def __init__(self, height):
@@ -66,46 +81,50 @@ def get_winning_condition(node):
   
 # Node handlers
 
-def handle_node_g(node, header, bodies, transform):
-    id, label = element_common(node)
+def handle_node_g(node, header, bodies, transform, label):
+    id, l = element_common(node)
+    label.push(l)
     t = get_transform(node)
     transform.push()
     transform.translate(t)
-    if 'multishape' in label:
+    if 'multishape' in l:
         mbodies = []
-        parse_children(node, header, mbodies, transform)
-        bodies.append((id, label, [shapes[0] for _, _, shapes in mbodies]))
+        parse_children(node, header, mbodies, transform, label)
+        bodies.append((id, label(), [shapes[0] for _, _, shapes in mbodies]))
     else:
-        parse_children(node, header, bodies, transform)
+        parse_children(node, header, bodies, transform, label)
     transform.pop()
+    label.pop()
 
-def handle_node_svg(node, header, bodies, transform):
+def handle_node_svg(node, header, bodies, transform, label):
     header.update(width=float(node.getAttribute('width')), 
                   height=float(node.getAttribute('height')),
                   winning_condition=get_winning_condition(node))
     transform = Transform(header['height'])
-    parse_children(node, header, bodies, transform)
+    parse_children(node, header, bodies, transform, label)
 
-def handle_node_namedview(node, header, bodies, transform):
+def handle_node_namedview(node, header, bodies, transform, label):
     bodies.append(('pagecolor', node.getAttribute('pagecolor')))
-    parse_children(node, header, bodies, transform)
+    parse_children(node, header, bodies, transform, label)
 
-def handle_node_rect(node, header, bodies, transform):
-    id, label, sd = shape_common(node)
+def handle_node_rect(node, header, bodies, transform, label):
+    id, l, sd = shape_common(node)
+    label.push(l)
     x, y, w, h = [float(node.getAttribute(n)) for n in ['x', 'y', 'width', 'height']]
-    bodies.append((id, label, [('rect', id, label, sd, (transform((x, y + h)), (w, h)))]))
+    bodies.append((id, label(), [('rect', id, label(), sd, (transform((x, y + h)), (w, h)))]))
+    label.pop()
 
-def handle_node_path(node, header, bodies, transform):
+def handle_node_path(node, header, bodies, transform, label):
+    id, l, sd = shape_common(node)
+    label.push(l)
     t = get_transform(node)
     transform.push()
     transform.translate(t)
     if node.getAttribute('sodipodi:type') == 'arc':
-        id, label, sd = shape_common(node)
         x, y, rx, ry = [float(node.getAttribute('sodipodi:'+n))
                         for n in ['cx', 'cy', 'rx', 'ry']]
-        bodies.append((id, label, [('circle', id, label, sd, (transform((x, y)), (rx, ry)))]))
+        bodies.append((id, label(), [('circle', id, label(), sd, (transform((x, y)), (rx, ry)))]))
     else:
-        id, label, sd = shape_common(node)
         path = node.getAttribute('d')
         path = linearize_path(path)
         # Make sure that closed paths are defined counter clockwise
@@ -113,7 +132,7 @@ def handle_node_path(node, header, bodies, transform):
             path = reverse_path(path)
         is_polygon = path.split()[-1] == 'z'
         name = 'polygon' if is_polygon else 'path'
-        if is_polygon and 'triangulate' in label:
+        if is_polygon and 'triangulate' in label():
             convex_path = triangulate_path(path)
             paths = split_paths(convex_path)
         else:
@@ -122,22 +141,22 @@ def handle_node_path(node, header, bodies, transform):
         for path in paths:
             points = path_points(path)
             points = [transform((x,y)) for x, y in points]
-            parts.append((name, id, label, sd, points))
-        #return [(name, id, label, sd, points)]
-        bodies.append((id, label, parts))
+            parts.append((name, id, label(), sd, points))
+        bodies.append((id, label(), parts))
     transform.pop()
-    
-def handle_node_default(node, header, bodies, transform):
+    label.pop()
+
+def handle_node_default(node, header, bodies, transform, label):
     #print node.nodeName
-    parse_children(node, header, bodies, transform)
+    parse_children(node, header, bodies, transform, label)
 
 # Level parser
 
-def parse_children(node, header, bodies, transform):
+def parse_children(node, header, bodies, transform, label):
     for child in node.childNodes:
-        parse_subtree(child, header, bodies, transform)
+        parse_subtree(child, header, bodies, transform, label)
 
-def parse_subtree(node, header, bodies, transform):
+def parse_subtree(node, header, bodies, transform, label):
     handlers = {
             'g': handle_node_g, 
             'svg': handle_node_svg, 
@@ -145,14 +164,15 @@ def parse_subtree(node, header, bodies, transform):
             'rect': handle_node_rect, 
             'sodipodi:namedview': handle_node_namedview
             }
-    handlers.get(node.nodeName, handle_node_default)(node, header, bodies, transform)
+    handlers.get(node.nodeName, handle_node_default)(node, header, bodies, transform, label)
 
 def read_level(file):
     root = minidom.parse(file)
     header = {}
     bodies = []
+    label = LabelStack()
 
-    parse_subtree(root, header, bodies, None)
+    parse_subtree(root, header, bodies, None, label)
     
     return header, bodies
 
